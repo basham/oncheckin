0c543c4cb989e0c87e437772ddd1ed7cde722633
Chris Basham <chris@bash.am>
2014-03-06 23:20:26 -0500

    Participant views list attended events.

--- app/modules/app/participant.js
+++ app/modules/app/participant.js
@@ -4,10 +4,28 @@ angular.module('oncheckinApp')
   .controller('AppParticipantCtrl', function ($scope, $firebase, firebaseRef, Firebase, $stateParams, $state, $modal) {
     
     // Grab the event.
-    var participantRef = firebaseRef('participants/' + $stateParams.id);
+    var participantRef = firebaseRef('participants').child($stateParams.id);
+    var attendancesRef = firebaseRef('attendances');
+    var eventsRef = firebaseRef('events');
+
+    // Initialize scope objects.
+    $scope.participant = $firebase(participantRef);
+
+    // Join the event's attendance and participant data.
+    var participantAttendancesRef = participantRef.child('attendances');
+    var participantAttendancesRef = Firebase.util.intersection(
+      participantAttendancesRef,
+      {
+        ref: attendancesRef,
+        keyMap: {
+          host: 'host',
+          event: eventsRef
+        }
+      });
+    $scope.attendances = $firebase(participantAttendancesRef);
 
     participantRef.once('value', function(snap) {
-      var chapterRef = firebaseRef('chapters/' + snap.val().chapter);
+      var chapterRef = firebaseRef('chapters').child(snap.val().chapter);
       $scope.chapter = $firebase(chapterRef);
 
       $scope.removeParticipant = function() {
@@ -22,14 +40,11 @@ angular.module('oncheckinApp')
             }
           })
           .result.then(function(model) {
-            chapterRef.child('participants/' + participantRef.name()).remove();
+            chapterRef.child('participants').child(participantRef.name()).remove();
             participantRef.remove();
             $state.transitionTo('app.chapter', { id: chapterRef.name() });
           });
       };
     });
-
-    // Initialize scope objects.
-    $scope.participant = $firebase(participantRef);
 
   });
